92daa6c6c10743091b0d507500e3ac45b98f6708
gotama@undiksha.ac.id <gotama@undiksha.ac.id>
2022-07-14 14:59:22 +0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:project_uts/screens/about.dart';
+import 'package:project_uts/screens/simple_map.dart';
 import 'package:project_uts/screens/home_screen.dart';
 
 void main() => runApp(MaterialApp(
@@ -8,6 +8,25 @@ void main() => runApp(MaterialApp(
       home: bottomNavbar(),
     ));
 
+// Login
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return ChangeNotifierProvider(
+//       create: (ctx) => Products(),
+//       builder: (context, child) => MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         home: LoginPage(),
+//         routes: {
+//           AddProductPage.route: (ctx) => AddProductPage(),
+//           EditProductPage.route: (ctx) => EditProductPage(),
+//         },
+//       ),
+//     );
+//   }
+// }
+
+//end login
 class bottomNavbar extends StatefulWidget {
   const bottomNavbar({Key? key}) : super(key: key);
 
@@ -20,7 +39,7 @@ class _bottomNavbarState extends State<bottomNavbar> {
 
   final screens = [
     HomeScreen(),
-    About(),
+    SimpleMapScreen(),
   ];
 
   void _onItemTapped(int index) {
@@ -38,7 +57,7 @@ class _bottomNavbarState extends State<bottomNavbar> {
       bottomNavigationBar: BottomNavigationBar(
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.account_box), label: 'About')
+          BottomNavigationBarItem(icon: Icon(Icons.location_city), label: 'Location')
         ],
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
